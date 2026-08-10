9ad15386e7f20fac1123de9c3a688eda09c67b29
kalya <macsoftechnologies@gmail.com>
2026-08-10 18:00:20 +0530

    lineman issues

--- src/complaints/staff-complaints.service.ts
+++ src/complaints/staff-complaints.service.ts
@@ -124,6 +124,8 @@ export class StaffComplaintsService {
       .leftJoinAndSelect('section.subdivision', 'subdivision')
       .leftJoinAndSelect('subdivision.division', 'division')
       .leftJoinAndSelect('division.circle', 'circle')
+      .leftJoinAndSelect('complaint.assigned_lineman', 'assigned_lineman')
+      .leftJoinAndSelect('assigned_lineman.staff', 'lineman_staff')
       .where('complaint.complaint_id = :id', { id });
 
     if (!isSuperAdmin && roleLevel !== 6) {
